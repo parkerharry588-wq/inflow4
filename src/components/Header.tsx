94b41d2f3572227977cebca447fd9b12327c5d93
parkerharry588-wq <parkerharry588@gmail.com>
2025-08-30 04:57:20 -0700

    Replace header logo with Inflow logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header({ currentPage = 'home', onNavigate }: HeaderProps
               className="flex items-center space-x-2 cursor-pointer"
               onClick={() => handleNavigation('home')}
             >
-              <img src="/public/Screenshot 2025-08-23 154231.png" alt="Inflow" className="h-10 w-auto" />
+              <img src="/public/dffdf.png" alt="Inflow Logo" className="h-10 w-auto" />
             </div>
 
             {/* Desktop Navigation */}
