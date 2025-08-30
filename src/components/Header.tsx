000016cad7760407c6273eb17b697cf5476d2799
parkerharry588-wq <parkerharry588@gmail.com>
2025-08-30 05:02:52 -0700

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,7 +46,7 @@ export default function Header({ currentPage = 'home', onNavigate }: HeaderProps
               className="flex items-center space-x-2 cursor-pointer"
               onClick={() => handleNavigation('home')}
             >
-              <img src="/public/dffdf.png" alt="Inflow Logo" className="h-auto w-30" />
+              <img src="/public/dffdf.png" alt="Inflow Logo" className="h-auto w-24" />
             </div>
 
             {/* Desktop Navigation */}
